f38ecc7fe2656931d35ea7698c52c3feb9dc6625
liujl <liujiulong@aliyun.com>
2019-09-29 22:35:56 +0800

    完善了user业务层

--- src/main/java/com/mars/elleshop/controller/UserController.java
+++ src/main/java/com/mars/elleshop/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.mars.elleshop.controller;
 
+import com.mars.elleshop.common.JsonBean;
+import com.mars.elleshop.entity.User;
+import com.mars.elleshop.service.UserService;
+import com.mars.elleshop.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.spring.web.json.Json;
 
-import javax.servlet.http.HttpSession;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author liujiulong
@@ -14,21 +19,26 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequestMapping(path = "/user")
 public class UserController {
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @RequestMapping("/login.do")
-//    public String login(String phone, String password , HttpSession session){
-//
-//        User user = userService.login(phone, password);
-//        session.setAttribute(StrUtils.LOGIN_INFO, user);
-//
-//        return "redirect:/secKill/list.do";
-//    }
-//
-//    @RequestMapping("/tologin")
-//    public String toLogin(){
-//        return "login";
-//    }
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping("/login.do")
+    public JsonBean login(String phone, String password){
+
+        //盐值
+        //生成token
+
+        String passwordMD5 = MD5Utils.md5(password + "liuxiaoqi");
+        User user = userService.login(phone, passwordMD5);
+
+        String token = MD5Utils.md5(phone + "liujiulong");
+        //将token放入到redis中
+        stringRedisTemplate.opsForValue().set(token, phone);
+        stringRedisTemplate.expire(token,1800, TimeUnit.SECONDS);
+        return new JsonBean<>(1, "登录成功");
+    }
 }
--- src/main/java/com/mars/elleshop/service/impl/UserServiceImpl.java
+++ src/main/java/com/mars/elleshop/service/impl/UserServiceImpl.java
@@ -1,8 +1,32 @@
 package com.mars.elleshop.service.impl;
 
+import com.mars.elleshop.dao.UserDao;
+import com.mars.elleshop.entity.User;
+import com.mars.elleshop.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @author liujiulong
  * @date 2019/09/29  21:05:00
  */
-public class UserServiceImpl {
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public User login(String phone, String password) {
+
+        User user = userDao.findByPhone(phone);
+        if (user == null){
+            throw new RuntimeException("手机号错误");
+        }
+        if (!password.equals(user.getPassword())){
+            throw new RuntimeException("密码错误");
+        }
+
+        return user;
+    }
 }
