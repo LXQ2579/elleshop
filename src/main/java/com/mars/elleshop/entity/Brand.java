bb28298856784340eced8cb3b13892fe2d375360
lufangzhang <lufang.zhang@aliyun.com>
2019-09-30 10:38:14 +0800

    品牌馆的首页展示

---
+++ src/main/java/com/mars/elleshop/entity/Brand.java
@@ -0,0 +1,51 @@
+package com.mars.elleshop.entity;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ *                     .::::.
+ *                  .::::::::.
+ *                 :::::::::::
+ *             ..:::::::::::'
+ *           '::::::::::::'
+ *             .::::::::::
+ *        '::::::::::::::..
+ *             ..::::::::::::.
+ *           ``::::::::::::::::
+ *            ::::``:::::::::'        .:::.
+ *           ::::'   ':::::'       .::::::::.
+ *         .::::'      ::::     .:::::::'::::.
+ *        .:::'       :::::  .:::::::::' ':::::.
+ *       .::'        :::::.:::::::::'      ':::::.
+ *      .::'         ::::::::::::::'         ``::::.
+ *  ...:::           ::::::::::::'              ``::.
+ * ```` ':.          ':::::::::'                  ::::..
+ *                    '.:::::'                    ':'````..
+ *         女神镇楼                 BUG辟易
+ *  佛曰:
+ *          写字楼里写字间，写字间里程序员；
+ *          程序人员写程序，又拿程序换酒钱。
+ *          酒醒只在网上坐，酒醉还来网下眠；
+ *          酒醉酒醒日复日，网上网下年复年。
+ *          但愿老死电脑间，不愿鞠躬老板前；
+ *          奔驰宝马贵者趣，公交自行程序员。
+ *          别人笑我忒疯癫，我笑自己命太贱；
+ *          只见满街漂亮妹，哪个得归程序员？
+ * @Description:
+ * @Author: lufang.zhang
+ * @Date: 2019/9/30
+ */
+@ApiModel(value = "品牌分类")
+@Data
+public class Brand implements Serializable {
+
+    @ApiModelProperty("品牌id")
+    private int brandId;
+    @ApiModelProperty("品牌名")
+    private String brandName;
+
+}
