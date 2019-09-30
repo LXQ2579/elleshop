2c659c282a322cd85eb927674760876c36bf897b
lei <2468549595@qq.com>
2019-09-30 11:57:33 +0800

    品类信息展示show category message

---
+++ src/main/java/com/mars/elleshop/dao/CategoryDao.java
@@ -0,0 +1,14 @@
+package com.mars.elleshop.dao;
+
+import com.mars.elleshop.entity.Category;
+
+import java.util.List;
+
+public interface CategoryDao {
+
+    /**
+     * 查找到所有分类展示到页面上
+     * @return
+     */
+    public List<Category> getAllKinds();
+}
