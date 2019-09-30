125488767257f8d3cb596ede37c4afe426249258
sanriyue <443807234@qq.com>
2019-09-30 14:11:31 +0800

    sanriyue

--- src/main/java/com/mars/elleshop/dao/ClassifyDao.java
+++ src/main/java/com/mars/elleshop/dao/ClassifyDao.java
@@ -1,10 +1,12 @@
 package com.mars.elleshop.dao;
 
 import com.mars.elleshop.entity.Classify;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author sanriyue
  */
+@Repository
 public interface ClassifyDao {
 
     Classify findClassifyById(Integer classifyId);
--- src/main/java/com/mars/elleshop/dao/GoodsTypeDao.java
+++ src/main/java/com/mars/elleshop/dao/GoodsTypeDao.java
@@ -1,12 +1,14 @@
 package com.mars.elleshop.dao;
 
 import com.mars.elleshop.entity.GoodsType;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * @author sanriyue
  */
+@Repository
 public interface GoodsTypeDao {
     GoodsType findGoodsTypeById(Integer goodsTypeId);
 
