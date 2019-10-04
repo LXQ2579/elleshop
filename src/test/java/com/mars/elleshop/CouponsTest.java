34f6580728082a8bc284ba0cc4c0eafe83554ece
sanriyue <443807234@qq.com>
2019-10-04 09:28:47 +0800

    德玛西亚万岁

--- src/test/java/com/mars/elleshop/CouponsTest.java
+++ src/test/java/com/mars/elleshop/CouponsTest.java
@@ -2,10 +2,13 @@ package com.mars.elleshop;
 
 import com.mars.elleshop.dao.CouponsDao;
 import com.mars.elleshop.entity.Coupons;
+import com.mars.elleshop.entity.User;
+import com.mars.elleshop.utils.JsonUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -13,6 +16,9 @@ import java.util.List;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CouponsTest {
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
     @Autowired
     CouponsDao couponsDao;
 
@@ -28,5 +34,14 @@ public class CouponsTest {
         for (Coupons allCoupon : allCoupons) {
             System.out.println(allCoupon);
         }
+
+    }
+    @Test
+    public void testRedis(){
+        User user = new User();
+
+        stringRedisTemplate.opsForHash().put(111+"coupons",1+"",
+                user.toString());
+
     }
 }
